c2d1c3e9addfe82d13ec87c7cdfb4ab80afa8faa
Boris <bfeen@mail.ru>
2020-06-12 13:54:35 +0300

    2.2.2 Added tests for components and check it

---
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import WelcomeScreen from "./welcome-screen.jsx";
+
+
+describe(`WelcomeScreen component tests`, ()=> {
+  it(`Render WelcomeScreen`, () => {
+    const tree = renderer
+      .create(<WelcomeScreen
+        errorsCount = {3}
+      />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
