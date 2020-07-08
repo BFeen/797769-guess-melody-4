5b30451ef78baf2832e38b0d5ab489ae0345b19d
Boris <bfeen@mail.ru>
2020-07-08 18:36:19 +0300

    5.1.3 Updated tests

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import App from "./app.jsx";
+import {App} from "./app.jsx";
 
 
 const questions = [
@@ -39,15 +39,53 @@ const questions = [
   },
 ];
 
-describe(`App component tests`, ()=> {
-  it(`Render App`, () => {
+describe(`Render App`, ()=> {
+  it(`WelcomeScreen rendering`, () => {
     const tree = renderer
       .create(<App
         errorsCount={3}
         questions={questions}
+        onUserAnswer={() => {}}
+        onWelcomeButtonClick={() => {}}
+        step={-1}
       />)
       .toJSON();
 
     expect(tree).toMatchSnapshot();
+  });
+
+  it(`GenreQuestionScreen rendering`, () => {
+    const tree = renderer
+      .create(<App
+        errorsCount={3}
+        questions={questions}
+        onUserAnswer={() => {}}
+        onWelcomeButtonClick={() => {}}
+        step={0}
+      />, {
+        createNodeMock: () => {
+          return {};
+        }
+      })
+      .toJSON();
+
+      expect(tree).toMatchSnapshot();
+  });
+
+  it(`ArtistQuestionScreen rendering`, () => {
+    const tree = renderer
+      .create(<App
+        errorsCount={3}
+        questions={questions}
+        onUserAnswer={() => {}}
+        onWelcomeButtonClick={() => {}}
+        step={1}
+      />, {
+        createNodeMock: () => {
+          return {};
+        }
+      }).toJSON();
+
+      expect(tree).toMatchSnapshot();
   });
 });
