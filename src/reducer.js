287525c4d43718a29652e2a41034fa501a9ae3b9
Boris <bfeen@mail.ru>
2020-07-08 19:39:40 +0300

    5.1.4 Added Mistakes component. Realized user answer checking and increment mistakes. Connected game-screen to Store

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,6 @@
 import {extend} from "./utils.js";
+import {GameType} from "./const.js";
+import { PureComponent } from "react";
 
 
 const initialState = {
@@ -11,11 +13,38 @@ const ActionType = {
   INCREMENT_STEP: `INCREMENT_STEP`,
 };
 
+const isArtistAnswerCorrect = (question, userAnswer) => {
+  return userAnswer.artist === question.song.artist;
+};
+
+const isGenreAnswerCorrect = (question, userAnswer) => {
+  return userAnswer.every((item, index) => {
+    return item === (question.answers[index].genre === question.genre);
+  });
+};
+
 const ActionCreator = {
   incrementStep: () => ({
     type: ActionType.INCREMENT_STEP,
     payload: 1,
   }),
+  incrementMistake: (question, userAnswer) => {
+    let answerIsCorrect = false;
+
+    switch (question.type) {
+      case GameType.ARTIST:
+        answerIsCorrect = isArtistAnswerCorrect(question, userAnswer);
+        break;
+      case GameType.GENRE:
+        answerIsCorrect = isGenreAnswerCorrect(question, userAnswer);
+        break;
+    }
+
+    return {
+      type: ActionType.INCREMENT_MISTAKES,
+      payload: answerIsCorrect ?  0 : 1,
+    };
+  },
 };
 
 const reducer = (state = initialState, action) => {
