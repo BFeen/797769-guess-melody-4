e290982fe4534b30d864e29d59035de0c97b51d4
Boris <bfeen@mail.ru>
2020-07-28 11:54:07 +0300

    7.1.5 Connected thunk, api and new functions to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
 import {Provider} from "react-redux";
 import App from "./components/app/app.jsx";
-import {reducer} from "./reducer.js";
+import reducer from "./reducer/reducer.js";
+import {Operation as DataOperation} from "./reducer/data/data.js";
+import {Operation as UserOperation, ActionCreator, AuthorizationStatus} from "./reducer/user/user.js";
+import {createAPI} from "./api.js";
 
 
-const store = createStore(reducer);
+const onAuthorized = () => {
+  store.dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH));
+};
+
+const api = createAPI(onAuthorized);
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk.withExtraArgument(api)),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
+);
+
+store.dispatch(DataOperation.loadQuestions());
+store.dispatch(UserOperation.checkAuth());
 
 ReactDOM.render(
     <Provider store={store}>
